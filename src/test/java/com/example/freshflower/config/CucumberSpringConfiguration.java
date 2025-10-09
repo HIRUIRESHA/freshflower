5295476dbe6fbc15cdb4cb2ec1ad68271c091925
Hiru_Iresha <hiruireshasewwandi@gmail.com>
2025-10-09 20:24:25 +0530

    updated ci-cd.yml

--- src/test/java/com/example/freshflower/config/CucumberSpringConfiguration.java
+++ src/test/java/com/example/freshflower/config/CucumberSpringConfiguration.java
@@ -2,11 +2,13 @@ package com.example.freshflower.config;
 
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 
 @CucumberContextConfiguration
 @SpringBootTest
 @ContextConfiguration(classes = TestConfig.class)
+@ActiveProfiles("test") // <-- add this line
 public class CucumberSpringConfiguration {
     // This is the ONLY configuration class
 }
