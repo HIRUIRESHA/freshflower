1f24c9b7b87fd3c56f9f35c32f8e36dc9b1c4406
Hiru_Iresha <hiruireshasewwandi@gmail.com>
2025-10-13 06:48:07 +0530

    new1

--- src/test/java/com/example/freshflower/config/CucumberSpringConfiguration.java
+++ src/test/java/com/example/freshflower/config/CucumberSpringConfiguration.java
@@ -3,12 +3,10 @@ package com.example.freshflower.config;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
 
 @CucumberContextConfiguration
 @SpringBootTest
-@ContextConfiguration(classes = TestConfig.class)
-@ActiveProfiles("test") // <-- add this line
+@ActiveProfiles("test")
 public class CucumberSpringConfiguration {
-    // This is the ONLY configuration class
+    // REMOVE THIS LINE: @ContextConfiguration(classes = TestConfig.class)
 }
