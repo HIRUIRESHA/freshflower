7bf1dd7a63758d1ce9df3568de8f31fcdbd98931
Hiru_Iresha <hiruireshasewwandi@gmail.com>
2025-10-13 06:07:56 +0530

    Update 4

--- src/test/java/com/example/freshflower/FreshflowerApplicationTests.java
+++ src/test/java/com/example/freshflower/FreshflowerApplicationTests.java
@@ -2,11 +2,14 @@ package com.example.freshflower;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class FreeInPowerApplicationTests {
+@ActiveProfiles("ci") // 👈 add this line
+class FreshflowerApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// This test just checks if Spring loads correctly.
 	}
 }
